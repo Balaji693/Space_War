5f3c552ee48264e912a579b2ef11fcbad2541100
BalajiKrishnan <balajivirat693@gmail.com>
2022-04-07 11:53:52 +0530

    Update

--- assets/Scripts/levelloader.ts
+++ assets/Scripts/levelloader.ts
@@ -4,12 +4,7 @@ const { ccclass, property } = _decorator;
 
 @ccclass('Levelloader')
 export class Levelloader extends Component {
-    // [1]
-    // dummy = '';
 
-    // [2]
-    // @property
-    // serializableDummy = 0;
 
     onLoad()
     {
@@ -41,13 +36,4 @@ export class Levelloader extends Component {
       }
 }
 
-/**
- * [1] Class member could be defined like this.
- * [2] Use `property` decorator if your want the member to be serializable.
- * [3] Your initialization goes here.
- * [4] Your update function goes here.
- *
- * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
- * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
- * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
- */
+
